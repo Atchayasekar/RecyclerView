86d6f2c1fb432845f48a72dd79d1234482db76ed
Atchayasekar <atchaya051997@gmail.com>
2019-12-05 23:33:21 +0530

    item movie card added

---
+++ app/src/main/java/com/atchaya/recyclerview/Adapters/TicketRecyclerAdapter.java
@@ -0,0 +1,22 @@
+package com.atchaya.recyclerview.Adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TicketRecyclerAdapter {
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        private
+
+    }
+
+
+
+
+}
